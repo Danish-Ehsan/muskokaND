800bb8446fac7d6c8d065afab3859f888c60f4a1
Danish <Danish@Danishs-MacBook-Pro.local>
2017-08-01 17:39:39 -0400

    added desktop_menu.js

---
+++ js/desktop_menu.js
@@ -0,0 +1,24 @@
+$(function() {
+
+	var $nav = $('#nav');
+	var $window = $(window);
+
+	setInterval(function() {
+		var scrollOrg = $window.scrollTop();
+		console.log('scrollOrg: ' + scrollOrg)
+
+		setTimeout(function () {
+			var scrollNew = $window.scrollTop();
+			console.log('scrollNew: ' + scrollNew)
+			if ((scrollNew - scrollOrg) > 100) {
+			$nav.slideUp();
+		} else if ((scrollNew - scrollOrg) < -100) {
+			$nav.slideDown();
+		}
+		}, 200);
+
+		
+
+	},250);
+
+});
