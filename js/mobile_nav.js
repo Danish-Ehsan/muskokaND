3d746e2aeed811f9d0d1434b657f2ebd0764e875
Danish <Danish@Danishs-MacBook-Pro.local>
2017-08-08 18:38:15 -0400

    Updated copy. Mobile button animation created. Various mobile tweaks. Nav now has Home on other pages.

--- js/mobile_nav.js
+++ js/mobile_nav.js
@@ -2,15 +2,12 @@ $(function () {
 
 	var $navList = $('#mobNav ul');
 	var $navButton = $("#navButton");
+	var $butLine = $('.navBtnLine');
 
 
 	$navButton.on('click', function() {
 		$navList.slideToggle();
+		$butLine.toggleClass('open');
 	});
-
-	$navList.on('click', function() {
-		$navList.slideToggle();
-	});
-
 
 });
